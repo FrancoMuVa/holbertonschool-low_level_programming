2e042aeb76596f7ec6267cc256e39f15deddbb58
FrancoMuVa <muvafranco@gmail.com>
2023-06-15 13:14:47 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,15 +9,19 @@
 int main(void)
 {
 	int num;
+	int ch;
 
+	ch = 44;
 	for (num = 48; num <= 57; num++)
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
-		if (num == 9)
-			putchar('$');
+
+		if (num == 57)
+			putchar(ch - 8);
+		else
+			putchar(ch);
 	}
+	putchar('\n');
 
 	return (0);
 }
