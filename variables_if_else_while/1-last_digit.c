e877387a0ae41087ce0cfa299db92c20023848dc
FrancoMuVa <muvafranco@gmail.com>
2023-06-15 07:01:48 -0700

    Last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ int main(void)
 	if (last_digit > 5)
 		printf("is %d and is greater than 5\n", last_digit);
 
-	else if (last_digit < 6)
-		printf("is %d and is less than 6 and not 0\n", last_digit);
+	else if (last_digit == 0)
+		printf("is %d and is 0\n", last_digit);
 
 	else
-		printf("is %d and is 0\n", last_digit);
+		 printf("is %d and is less than 6 and not 0\n", last_digit);
 
 	return (0);
 }
