cddc03c6aa6c0609bb546eb2a336bf65b36f00ab
FrancoMuVa <muvafranco@gmail.com>
2023-06-15 14:06:23 -0700

    Combination

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, j, var_i, var_j;
+
+	var_i = 48;
+	var_j = 49;
+
+	for (i = var_j; i < 57; i++)
+	{
+		for (j = var_j; j <= 57; i++)
+		{
+			putchar(i);
+			putchar(j);
+
+			if (i != 56 && j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			if (j == 57)
+				var_j++;
+		}
+		var_i++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
