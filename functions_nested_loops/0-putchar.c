9dd6710dad31c2921dcb1bea59ec5b31f9fa9321
FrancoMuVa <muvafranco@gmail.com>
2023-06-16 10:55:53 -0700

    _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriatley.
+ */
+
+int main(void)
+{
+	_putchar('h');
+	_putchar('o');
+	_putchar('l');
+	_putchar('a');
+
+	return (0);
+}
