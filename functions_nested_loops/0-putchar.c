4cc523754a25c9dc6581b343590cd7e9954ee3ca
FrancoMuVa <muvafranco@gmail.com>
2023-06-16 10:58:48 -0700

    _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -9,10 +9,14 @@
 
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
 	_putchar('h');
-	_putchar('o');
-	_putchar('l');
 	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
