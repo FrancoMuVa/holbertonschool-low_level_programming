ee1ae2a86cb0215d014e0a7593b083aa028e4dc8
FrancoMuVa <muvafranco@gmail.com>
2023-06-18 13:11:29 -0700

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/*
+ * _islower - chek the code.
+ * @c: First operand
+ *
+ * Return: Always 0. (Sucaccess)
+ */
+
+int _islower(int c)
+{
+	int ch;
+
+	ch = 96;
+	while (ch <= 122 || ch == c)
+	{
+		ch++;
+
+		if (ch == c)
+			return (1);
+	}
+	if (ch != c)
+		return (0);
+
+	return (0);
+}
