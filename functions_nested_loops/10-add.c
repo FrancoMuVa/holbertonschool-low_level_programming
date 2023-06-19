f71ff760aeb6a8ddab02fbf0b93d1b1cd2028ba0
FrancoMuVa <muvafranco@gmail.com>
2023-06-19 06:10:39 -0700

    Add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @n: First integer.
+ * @i: Second integer.
+ *
+ * Return: Always 0. (Sucsaccess)
+ */
+
+int  add(int n, int i)
+{
+	int _add;
+
+	_add = n + i;
+
+	return (_add);
+
+
+	return (0);
+}
