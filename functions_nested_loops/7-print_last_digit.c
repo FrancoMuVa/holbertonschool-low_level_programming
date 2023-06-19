4abca6b4b5ce15c2f54f5ab0bbdff44bfbb7f02f
FrancoMuVa <muvafranco@gmail.com>
2023-06-18 17:39:19 -0700

    Last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Print last digit of a number.
+ * @n: integer.
+ *
+ * Return: Always 0. (Sucaccess)
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n = (n) * (-1);
+
+	n = n % 10;
+	_putchar(n + '0');
+	return (n);
+
+	return (0);
+}
