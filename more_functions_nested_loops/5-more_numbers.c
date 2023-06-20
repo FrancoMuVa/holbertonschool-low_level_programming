39835ec9053e739dd2ef8940dafc2e4c9228636d
FrancoMuVa <muvafranco@gmail.com>
2023-06-20 11:19:57 -0700

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -7,19 +7,21 @@
 
 void more_numbers(void)
 {
-	int i, j, d1;
+	int i, j, d1, d2;
 
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 9)
+			d2 = j;
+
+			if (j >= 10)
 			{
 				d1 = j / 10;
-				j = j % 10;
+				d2 = j % 10;
 				_putchar(d1 + '0');
 			}
-			_putchar(j);
+			_putchar(d2 + '0');
 		}
 
 		_putchar('\n');
