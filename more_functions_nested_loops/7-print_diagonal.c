66d9377172d47dbed1691b229bb1b7e503b22955
FrancoMuVa <muvafranco@gmail.com>
2023-06-20 12:46:13 -0700

    diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -12,14 +12,14 @@ void print_diagonal(int n)
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				if (i == j)
+				if (j == i)
 					_putchar('\\');
-				else
-					_putchar(' ');
+
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
