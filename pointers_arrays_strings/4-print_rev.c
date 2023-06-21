fd1f9e380defce3d5630e300e229a0724f5a5891
FrancoMuVa <muvafranco@gmail.com>
2023-06-21 12:36:32 -0700

    print rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - Print a string in reverse.
+ * @s: String.
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i, count;
+
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		count++;
+
+	for (i = count; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
