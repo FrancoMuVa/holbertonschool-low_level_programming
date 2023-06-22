9395577a2a9906d269f45fe6b9c28c40fc70ffb3
FrancoMuVa <muvafranco@gmail.com>
2023-06-22 14:54:04 -0700

    Strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - Copies string in buffer.
+ * @dest: Pointer buffrr.
+ * @src: Pointer string.
+ *
+ * Return: dest.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i, c;
+	char *cpy = dest, *orig = src;
+
+	c = 0;
+	for (i = 0; src[i] != '\0'; i++)
+		c++;
+
+	for (i = 0; i <= c; i++)
+	{
+		*cpy = *orig;
+		cpy++;
+		orig++;
+	}
+
+	cpy++;
+	*cpy = '\0';
+
+	return (dest);
+}
