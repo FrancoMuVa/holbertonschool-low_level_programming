ecdceb7a00467678c4c3ff877cbddebdbdfe2b21
FrancoMuVa <muvafranco@gmail.com>
2023-06-22 12:17:28 -0700

    puts half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - Print a half of a string.
+ * @str: String.
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i, c, n;
+
+	c = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		c++;
+
+	if (c % 2 == 0)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			if (i >= (c / 2))
+				_putchar(str[i]);
+		}
+	}
+	else
+	{
+		n = (c - 1) / 2;
+		c = c - n;
+		for (i = n; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
