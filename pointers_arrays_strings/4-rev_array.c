c47c16107830d5137f74cf82e5812d6d4b208f8a
FrancoMuVa <muvafranco@gmail.com>
2023-06-26 08:18:37 -0700

    rev_str

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array.
+ * @a: Pointer to integer.
+ * @n: Integer.
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int i, piv, aux = n - 1;
+
+	for (i = 0; i <= n / 2; i++)
+	{
+		piv = a[aux - i];
+		a[aux - i] = a[i];
+		a[i] = piv;
+	}
+}
