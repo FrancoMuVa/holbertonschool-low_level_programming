3100b0396165fc58f2e055106328f08617244004
FrancoMuVa <muvafranco@gmail.com>
2023-06-26 06:51:52 -0700

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
+	while (s1[i] != '\0')
+		i++;
 
-	for (j = 0; s2[j] != '\0'; j++)
+	while (s2[j] != '\0')
+		j++;
 
 	if (i < j)
 		return (-1);
