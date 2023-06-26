c5d3d9babf5eab69c9391894023d73f6cdee8b70
FrancoMuVa <muvafranco@gmail.com>
2023-06-26 13:45:01 -0700

    leet

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * leet - Encodes a string.
@@ -10,15 +11,17 @@
 char *leet(char *s)
 {
 	char num[5] = {"43071"};
-	char let[5] = {"AEOTL"};
+	char let[5] = {"aeotl"};
 	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j <= 4; j++)
 		{
-			if (s[i] == let[j] || (s[i] + 32) == let[j])
+			if (s[i] - 32 == let[j] || s[i] == let[j])
+			{
 				s[i] = num[j];
+			}
 		}
 	}
 
