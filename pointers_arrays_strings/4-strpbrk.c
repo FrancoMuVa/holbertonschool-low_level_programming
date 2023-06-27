6db3f44aef0d89cca6a578b8e94f2f93d238aa14
FrancoMuVa <muvafranco@gmail.com>
2023-06-27 13:18:30 -0700

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: Poiniter to string.
+ * @accept: Pointer to string.
+ *
+ * Return: Pointer to char.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j, flag = 0;
+	char *p = s;
+
+	i = 0;
+	while (s[i] != '\0' && flag == 0)
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				flag = 1;
+				break;
+			}
+			j++;
+		}
+		p++;
+		i++;
+	}
+	p = p - 1;
+	return (p);
+}
