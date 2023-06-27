43ed5fcce77ccfa44db64ee6914208fe497a143f
FrancoMuVa <muvafranco@gmail.com>
2023-06-27 13:55:56 -0700

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring.
+ * @haystack: Pointer to string.
+ * @needle: Pointer to string.
+ *
+ * Return: Pointer type char.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, c;
+	char *s = haystack;
+
+	i = 0;
+	while (haystack[i] != '\0')
+	{
+		j = 0;
+		c = i;
+		while (needle[j] != '\0' && haystack[c] == needle[j])
+		{
+			c++;
+			j++;
+		}
+		if (needle[j] == '\0')
+			break;
+		
+		i++;
+		s++;
+	}
+
+	if (haystack[i] == '\0')
+		s = NULL;
+
+	return (s);
+}
