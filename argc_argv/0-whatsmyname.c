531f44ced38408af2d8b69c5ac514b247c1ce09e
FrancoMuVa <muvafranco@gmail.com>
2023-06-29 16:26:46 -0700

    My name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Print its name.
+ * @argc: Integer.
+ * @argv: Array containg the program line arguments.
+ *
+ * Return: 0.
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
