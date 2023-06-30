36af9fb3d1effb794eb88822615eba6841a6e718
FrancoMuVa <muvafranco@gmail.com>
2023-06-30 05:26:49 -0700

    mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Multiplies two numbers.
+ * @argc: Integer.
+ * @argv: Array.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+
+	if (argc - 1 == 2)
+	{
+		n1 = atoi(argv[2]);
+		n2 = atoi(argv[1]);
+		printf("%d\n", n1 * n2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
