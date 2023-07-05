5edc9c91a107aeec560ecd6e73c006dcf22249b6
FrancoMuVa <muvafranco@gmail.com>
2023-07-05 06:55:30 -0700

    strdup

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -13,24 +13,20 @@ char *_strdup(char *str)
 	int i = 0, c = 0;
 	char *p;
 
+	for (c = 0; str[c] != '\0'; c++)
+	
 	if (str == NULL)
 		return (NULL);
 
-	for (c = 0; str[c] != '\0'; c++)
-
-	p = malloc((c) * sizeof(char));
+	p = malloc((c + 1) * sizeof(char));
 
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-	else
-	{
 		for (i = 0; i <= c; i++)
 		{
 			p[i] = str[i];
 		}
-	}
-
 	return (p);
 }
