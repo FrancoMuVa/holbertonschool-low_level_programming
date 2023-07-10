6b1bb88945fd9f94a3fdb81ccccc0461424071dc
FrancoMuVa <muvafranco@gmail.com>
2023-07-10 13:53:40 -0700

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef	HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - New type.
+ * @name: Member of name.
+ * @age: Member of age.
+ * @owner: Member of owner.
+ *
+ * Description: Longer description.
+ */
+
 struct dog
 {
 	char *name;
