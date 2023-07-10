4823a659fe857d70e4b3d6412d53461b24df1771
FrancoMuVa <muvafranco@gmail.com>
2023-07-10 15:04:09 -0700

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -17,6 +17,23 @@ struct dog
 	char *owner;
 };
 
+/**
+ * struct dog_t - New type.
+ * @name: Member of name.
+ * @age: Member of age.
+ * @owner: Member of owner.
+ *
+ *
+ * Description: Longer description.
+ */
+
+struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
