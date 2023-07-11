b681efa8f110391e847b53220e9d8c159ce7389c
FrancoMuVa <muvafranco@gmail.com>
2023-07-11 07:46:00 -0700

    array iterator

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - Execute a function on ech element of array.
+ * @array: Pointer.
+ * @size: Size_t.
+ * @action: Pointer.
+ *
+ * Return: Nothing.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i <= size; i++)
+	{
+		action(array[i]);
+	}
+}
