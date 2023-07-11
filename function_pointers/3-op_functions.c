80db7d9f3443279d8559a4101daf25d6dc3a26df
FrancoMuVa <muvafranco@gmail.com>
2023-07-11 11:50:28 -0700

    op

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add.
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: Integer.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Return the difference.
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: Integer.
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Return the product.
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: Integer.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Return the result of division.
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: Integer.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Return the remainder of the division.
+ * @a: Integer.
+ * @b: Integer.
+ *
+ * Return: Integer.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
