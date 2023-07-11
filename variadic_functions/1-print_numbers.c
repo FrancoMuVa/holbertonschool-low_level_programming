a18f4c92361e52438b1b1f046f0f77a3b824cf0a
FrancoMuVa <muvafranco@gmail.com>
2023-07-11 16:20:29 -0700

    print numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Prints numbers.
+ * @separator: Pointer.
+ * @n: Unsigned int.
+ *
+ * Return: Nothing.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+
+	printf("%d", va_arg(args, int));
+	for (i = 1; i < n; i++)
+	{
+		if (separator != NULL)
+			printf("%s ", separator);
+		printf("%d", va_arg(args, int));
+	}
+	printf("\n");
+
+	va_end(args);
+}
