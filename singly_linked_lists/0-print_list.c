7be9cdf197951f340caa09846cfcd11114d33fec
FrancoMuVa <muvafranco@gmail.com>
2023-07-24 15:25:18 -0700

    feat: Task 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - Prints all elements of a list.
+ * @h: pointer to the list.
+ *
+ * Return: Number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+/*	list_t *pointer = h;
+*/	int elements = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		elements += 1;
+		h = h->next;
+	}
+	return (elements);
+}
