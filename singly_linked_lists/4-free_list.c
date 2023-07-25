e91112676557c45c1f426aad807c57d0821955bf
FrancoMuVa <muvafranco@gmail.com>
2023-07-25 14:56:22 -0700

    fix: valgrind errors

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
  * free_list - Frees a linked list.
  * @head: pointer.
@@ -12,11 +12,16 @@ void free_list(list_t *head)
 {
 	struct list_s *tmp;
 
+	if (head == NULL)
+		return;
+
 	while (head->next != NULL)
 	{
 		tmp = head;
 		head = head->next;
-		free(tmp->str);
+		if (tmp->str != NULL)
+			free(tmp->str);
 		free(tmp);
 	}
+	free(head);
 }
