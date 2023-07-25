7e9a991a586060f7955dd243c2e7f2e6445d88b1
FrancoMuVa <muvafranco@gmail.com>
2023-07-25 13:47:29 -0700

    feat: function to free a linked list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees a linked list.
+ * @head: pointer.
+ *
+ * Return: Nothing.
+ */
+
+void free_list(list_t *head)
+{
+	struct list_s *tmp;
+
+	while (head->next != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
