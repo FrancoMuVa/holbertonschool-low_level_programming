c3593f8078864430c5f4f4bb969624adcf2bc420
FrancoMuVa <muvafranco@gmail.com>
2023-07-25 11:56:19 -0700

    feat: function add node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - Function to add a new node at the beginning.
+ * @head: pointer to pointer.
+ * @str: string.
+ *
+ * Return: Address of the new element.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t *));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
