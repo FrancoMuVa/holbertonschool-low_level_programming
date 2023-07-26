ff940b1fc982c50affb72088baf40f0673fea0dc
FrancoMuVa <muvafranco@gmail.com>
2023-07-26 01:06:10 -0700

    fix: fixed bugs

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,8 +13,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
 
+	if (new == NULL)
+		return (NULL);
+
 	new->n = n;
 	new->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		new->next = NULL;
+		return (new);
+	}
 	new->next = *head;
 	*head = new;
 
