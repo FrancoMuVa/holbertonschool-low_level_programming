58b2462d672a808b0240e68bc88177473b6d16f8
FrancoMuVa <muvafranco@gmail.com>
2023-07-26 15:34:22 -0700

    feat: function to free a node

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Deletes the node at the index of a linked list.
+ * @head: pointer to pointer.
+ * @index: constant integer.
+ *
+ * Return: 1 if success, -1 otherwhithe.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int i = 0;
+	struct dlistint_s *tmp = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	tmp = *head;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		i += 1;
+	}
+
+	if (i == index)
+	{
+		tmp->prev->next = tmp->next;
+		tmp->next->prev = tmp->prev;
+		free(tmp);
+		return (1);
+	}
+	else
+		return (-1);
+}
