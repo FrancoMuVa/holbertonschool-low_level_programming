71936d73e145ab03a10309da9f0dc2897b599c1d
FrancoMuVa <muvafranco@gmail.com>
2023-07-26 07:13:13 -0700

    feat: function to add a node

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,45 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - Adds a new node at a given position.
+ * @h: pointer to pointer.
+ * @idx: unsigned int.
+ * @n: int.
+ *
+ * Return: Address of the new node.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	struct dlistint_s *tmp;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	if (*h == NULL)
+	{
+		new->prev = NULL;
+		new->next = NULL;
+		*h = new;
+		return (new);
+	}
+
+	tmp = *h;
+	while (i < idx && tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		i += 1;
+	}
+
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev = new;
+	tmp = new->prev;
+	tmp->next = new;
+
+	return (new);
+}
