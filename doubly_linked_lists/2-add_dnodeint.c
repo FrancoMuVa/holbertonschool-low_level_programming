5713ce00d0647599604470a7eb2434a36c6c0dba
FrancoMuVa <muvafranco@gmail.com>
2023-07-26 00:48:02 -0700

    feat: function to add a new node at de beginning

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - Adds a new node at the beginning.
+ * @head: pointer to pointer.
+ * @n: const integer.
+ *
+ * Return: Address of the new node.
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
