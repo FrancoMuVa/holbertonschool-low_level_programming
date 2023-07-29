ffb46222e0805bc75d18aac5ef1040615cbda8ba
FrancoMuVa <muvafranco@gmail.com>
2023-07-29 13:00:19 -0700

    feat: sets the value of a bit to 0 at a given index

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * clear_bit - Sets a specific bit to 0.
+ * @n: pointer.
+ * @index: unsigned int.
+ *
+ * Return: 1 (Success), -1 Otherwise.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num = *n;
+	unsigned int j = 0;
+	int i = (sizeof(n) * 8 - 1), mult = 1, mask = 2147483646;
+
+	if (num == 0)
+		return (1);
+	else if (index == 0)
+	{
+		num = (num & mask);
+		*n = num;
+		return (1);
+	}
+
+	while (i >= 0)
+	{
+		if ((num >> i) & 1)
+			break;
+		i--;
+	}
+
+	while (j <= sizeof(n) * 8 - 1)
+	{
+		if (j++ == index)
+		{
+			num = (num & mask);
+			*n = num;
+			return (1);
+		}
+		mult *= 2;
+		mask -= mult;
+		i--;
+	}
+	return (-1);
+}
