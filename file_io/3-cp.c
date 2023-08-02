73bd183ce92d6adcba818bfc5ed6d2937b117eac
FrancoMuVa <muvafranco@gmail.com>
2023-08-02 09:23:35 -0700

    fix: fixed bugs

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fd, fd2, byt_rd, cls;
+	int fd, fd2, byt_rd, cls, byt_wr;
 	char *buff = (char *)malloc(sizeof(char) * 1024);
 
 	if (buff == NULL)
@@ -23,32 +23,32 @@ int main(int argc, char *argv[])
 		byt_rd = read(fd, buff, 1024);
 		if (fd < 0 || byt_rd < 0)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		fd2 = open(argv[2], O_CREAT | O_EXCL | O_WRONLY, 0664);
 		if (fd2 < 0)
 			fd2 = open(argv[2], O_TRUNC | O_WRONLY);
-		if (fd2 < 0)
+		byt_wr = write(fd2, buff, byt_rd);
+		if (fd2 < 0 || byt_wr < 0)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99); }
-		write(fd2, buff, byt_rd);
 		cls = close(fd);
 		if (cls < 0)
 		{
-			dprintf(2, "Error: Can't close fd %d\n", fd);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 			exit(100); }
 		cls = close(fd2);
 		if (cls < 0)
 		{
-			dprintf(2, "Error: Can't close fd %d\n", fd2);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
 			exit(100); }
 	}
 	else
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97); }
 	free(buff);
-	return (1);
+	return (0);
 }
