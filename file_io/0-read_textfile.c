999f71b7537a67f95ac8be5b540b380902cff967
FrancoMuVa <muvafranco@gmail.com>
2023-08-02 05:28:25 -0700

    fix: fixed bugs

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,42 +10,18 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buff;
-	int fd = 0, byt_read = 0, byt_write = 0;
+	char buff[10000];
+	int fd = 0, byt_read = 0;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	printf("\nread: %d\n", byt_read);
-	buff = (char *)malloc(letters);
-	if (buff == NULL)
-	{
-		close(fd);
-		return (0);
-	}
 
 	byt_read = read(fd, buff, letters);
-	if (byt_read < 0)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
-
-	printf("\nread: %d\n", byt_read);
-	byt_write = write(STDOUT_FILENO, buff, byt_read);
-
-	printf("\nread: %d write: %d\n", byt_read, byt_write);
-	if (byt_read != byt_write)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
+	write(STDOUT_FILENO, buff, byt_read);
 	close(fd);
-	free(buff);
 	return (byt_read);
 }
