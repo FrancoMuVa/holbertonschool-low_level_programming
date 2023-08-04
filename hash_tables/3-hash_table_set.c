2f655a7336660bdb4a20ff91825db243e75428c0
FrancoMuVa <muvafranco@gmail.com>
2023-08-03 17:42:38 -0700

    fix: fixed bugs

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -28,8 +28,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int hash;
 	hash_node_t *tmp, *new = malloc(sizeof(hash_node_t));
 
-	if (!new || strcmp(key, "") || key == NULL || ht == NULL)
-		return (1);
+	if (new == NULL || strcmp(key, "") || key == NULL || ht == NULL)
+		return (0);
 
 	hash = (hash_djb2((const unsigned char *)key) % ht->size);
 
@@ -47,7 +47,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			new->next = tmp->next;
 			ht->array[hash] = new;
 			_free(tmp);
-			return (0);
+			return (1);
 		}
 		while (tmp->next != NULL && strcmp(tmp->next->key, key) != 0)
 			tmp = tmp->next;
@@ -63,5 +63,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			ht->array[hash] = new;
 		}
 	}
-	return (0);
+	return (2);
 }
