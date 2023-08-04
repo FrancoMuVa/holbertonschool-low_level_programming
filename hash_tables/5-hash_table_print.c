daba798ee4ab34ca79649aef2745f803f79e602b
FrancoMuVa <muvafranco@gmail.com>
2023-08-03 18:21:18 -0700

    feat: function to prints a hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: hash table.
+ *
+ * Return: Nothing.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (!ht)
+		return;
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		tmp = ht->array[index];
+		if (!tmp)
+		{
+			while (tmp)
+			{
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				tmp = tmp->next;
+				if (tmp->next)
+					printf(", ");
+			}
+		}
+	}
+	printf("}");
+}
